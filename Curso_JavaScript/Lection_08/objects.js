b07d1424b9578c5d8e2dc191422b359d082b91b9
josesalopasog <josesalopaso.13@gmail.com>
2024-11-07 14:15:20 -0500

    Updating the objects.js file

--- Curso_JavaScript/Lection_08/objects.js
+++ Curso_JavaScript/Lection_08/objects.js
@@ -2,14 +2,25 @@
 //Un objeto puede tener propiedades y metodos 
 
 let persona = {
-    //Propiedades: 
+    //Atributos: 
     nombre: 'Juan',
     apellido: 'Perez',
+    apellido2: 'Garcia',
     email: 'jperez@gmail.com',
     edad: 28,
+    idioma: 'es',
     //Metodos:
     nombreCompleto: function(){
         return this.nombre +' '+ this.apellido
+    },
+    get nombreCompleto2(){ //Con Get JS entiende que esto obtiene información de nuestro objeto
+        return this.nombre+' '+this.apellido+' '+this.apellido2;
+    },
+    get lang(){
+        return this.idioma.toUpperCase();
+    },
+    set lang(lang){
+        this.idioma = lang.toUpperCase();
     }
 }
 
@@ -19,7 +30,7 @@ console.log(persona.nombreCompleto())
 console.log(persona['email'])
 
 persona.tel = '1234567'
-
+delete persona.tel;
 
 //Iterar un objeto 
 for (nombrePropiedad in persona){
@@ -27,10 +38,32 @@ for (nombrePropiedad in persona){
     console.log(persona[nombrePropiedad])
 }
 
-
 //Otra forma de definir objetos 
 let persona2 = new Object();
 persona2.nombre = 'Carlos';
 persona2.apellido = 'Ramirez';
 persona2.direccion = 'Cra123 # 456';
 persona2.tel = '123456';
+
+//Formas de imprimir un objeto 
+
+//Concatenando
+console.log(persona.nombre +','+persona.apellido)
+
+//Iterar un objeto 
+for (nombrePropiedad in persona){
+    console.log(nombrePropiedad);
+    console.log(persona[nombrePropiedad]);
+}
+//Con un arreglo 
+let personaArray = Object.values(persona);
+console.log(personaArray);
+
+let personaString = JSON.stringify(persona);
+console.log(personaString);
+
+//Metodos Get y Set 
+console.log(persona.nombreCompleto2);
+console.log(persona.lang);
+persona.lang = 'en';
+console.log(persona.idioma);
