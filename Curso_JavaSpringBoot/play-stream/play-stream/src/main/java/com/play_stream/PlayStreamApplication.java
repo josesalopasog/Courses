f97fa5d6a5bc3d839786ef731532ffe8b6282ddf
josesalopasog <josesalopaso.13@gmail.com>
2026-01-10 22:30:16 -0500

    Java Spring Boot course 10/01/2026: Initial commit 🚀

---
+++ Curso_JavaSpringBoot/play-stream/play-stream/src/main/java/com/play_stream/PlayStreamApplication.java
@@ -0,0 +1,13 @@
+package com.play_stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayStreamApplication.class, args);
+	}
+
+}
---
+++ Curso_JavaSpringBoot/play-stream/play-stream/src/test/java/com/play_stream/PlayStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.play_stream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
