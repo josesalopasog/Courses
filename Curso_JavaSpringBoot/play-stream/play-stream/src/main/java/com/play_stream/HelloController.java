cfb939bae88c37e557f64403e96468d6008028a8
josesalopasog <josesalopaso.13@gmail.com>
2026-01-11 21:25:18 -0500

    Java course SpringBoot 11/01/2026 📚: configuration of application.properties and profiles

--- Curso_JavaSpringBoot/play-stream/play-stream/src/main/java/com/play_stream/HelloController.java
+++ Curso_JavaSpringBoot/play-stream/play-stream/src/main/java/com/play_stream/HelloController.java
@@ -1,19 +1,22 @@
 package com.play_stream;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    private final String platform;
     private final StreamPlayAiService aiService;
 
-    public HelloController(StreamPlayAiService aiService) {
+    public HelloController( @Value("${spring.application.name}") String platform, StreamPlayAiService aiService) {
+        this.platform = platform;
         this.aiService = aiService;
     }
 
     @GetMapping("/")
     public String hello(){
-        return this.aiService.generateGreeting();
+        return this.aiService.generateGreeting(platform);
     }
 }
--- Curso_JavaSpringBoot/play-stream/play-stream/src/main/java/com/play_stream/StreamPlayAiService.java
+++ Curso_JavaSpringBoot/play-stream/play-stream/src/main/java/com/play_stream/StreamPlayAiService.java
@@ -1,12 +1,13 @@
 package com.play_stream;
 
 import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.V;
 import dev.langchain4j.service.spring.AiService;
 
 @AiService
 public interface StreamPlayAiService {
     @UserMessage("""
-            Generate a friendly welcome greeting for the app StreamPlay in English, using fewer than 120 characters.
+            Generate a friendly welcome greeting for the app {{platform}} in English, using fewer than 120 characters.
             """)
-    String generateGreeting();
+    String generateGreeting(@V("platform") String platform);
 }
